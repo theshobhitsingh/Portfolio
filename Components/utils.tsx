ca73bc1ad34b6738dc6fb78a1de1951c954cff47
atif2fitnearn <atif.ansari@fitnearn.com>
2025-02-05 12:47:24 +0530

    Added Backround Effect

--- Components/utils.tsx
+++ Components/utils.tsx
@@ -1,5 +1,6 @@
-import { clsx } from 'clsx';
+import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-export function cn(...inputs) {
+
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
