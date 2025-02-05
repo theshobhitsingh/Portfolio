1f8c9eecd7a435b78ac8d9d3cd612c3aa5d303a0
Shobhit Singh <theshobhitt@gmail.com>
2025-02-05 13:34:54 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       </p>
       <p className="mt-2 flex items-center justify-center">
         <FaRocket className="mr-2 text-xl" />
-        &copy; 2️⃣0️⃣2️⃣5️⃣
+        &copy; ②⓪②⑤
       </p>
     </div>
   );
